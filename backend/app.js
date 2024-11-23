6b11b96778bf297e384787eacd2ad07267634aab
kothapallivinaykumar <165812777+vinay2902H@users.noreply.github.com>
2024-11-23 09:32:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ import prescriptionRouter from "./router/prescriptionRouter.js"
 const app = express();
 config({ path: "./config.env" });
 
-const allowedOrigins = ["https://hospital-admins.netlify.app", "http://localhost:3001","http://localhost:3002"];
+const allowedOrigins = ["https://hospital-admins.netlify.app", "https://hospitaldoctors.netlify.app","https://hospital-offical.netlify.app"];
 
 app.use(cors({
   origin: function (origin, callback) {
